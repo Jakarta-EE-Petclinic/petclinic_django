bd8c3421489b0aa1d60a30cf7a9f672cd239b3e7
thomaswoehlke <thomas.woehlke@gmail.com>
2022-03-26 16:23:03 +0100

    work

--- petclinic/views.py
+++ petclinic/views.py
@@ -1,10 +1,29 @@
-# Create your views here.
 from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'petclinic/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'petclinic/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'petclinic/results.html'
 
 
 def index(request):
