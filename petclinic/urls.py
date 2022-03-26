30978647086b1a667fb32977a6a83c98b3e1514a
thomaswoehlke <thomas.woehlke@gmail.com>
2022-03-26 16:02:35 +0100

    work

--- petclinic/urls.py
+++ petclinic/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'polls'
+app_name = 'petclinic'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
