8a0885168e3d701831d34de124bd8d12e94f90b6
thomaswoehlke <thomas.woehlke@gmail.com>
2022-03-27 13:12:18 +0200

    work

--- petclinic/views.py
+++ petclinic/views.py
@@ -1,9 +1,9 @@
 from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Choice, Question
 
